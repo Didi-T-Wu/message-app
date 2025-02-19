6791c50c4e783fa39bbc01b2ddbef33cd14ab557
Tichin Wu <twu38@mail.ccsf.edu>
2025-02-18 17:03:44 -0800

    Add initail form for logining in

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -1,9 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Login = ()=> {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
 
-  return <div>Login</div>
+
+  const onUsernameChange=(e)=>{
+    setUsername(e.target.value)
+
+  }
+
+  const onPasswordChange=(e)=>{
+    setPassword(e.target.value)
+
+  }
+
+  const onFormSubmit=(e)=>{
+    e.preventDefault()
+
+  }
+
+  return(<div>
+    <form onSubmit={onFormSubmit}>
+      <label htmlFor='username'>Username</label>
+      <input
+       type="text"
+       name="text"
+       id="username"
+       value={username}
+       onChange={onUsernameChange}
+       placeholder="Type your username"
+      >
+      </input>
+      <label htmlFor='password'>Password</label>
+      <input
+       type="password"
+       name="password"
+       id='password'
+       value={password}
+       onChange={onPasswordChange}
+       placeholder="Type your password"
+      >
+      </input>
+      <button>Login</button>
+    </form>
+    <br/> or
+    {/* FIXME:  react router to sign in page*/}
+    <div>Create an Account (To sign in page)</div>
+  </div>)
+
 }
 
 export default Login;
