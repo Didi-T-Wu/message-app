3d884731e00b2d766f8d677b5400430ebb072cbf
Tichin Wu <twu38@mail.ccsf.edu>
2025-03-03 15:59:37 -0800

    feat: adding auth context to ease the authentication process

---
+++ client/src/AuthProvider.js
@@ -0,0 +1,57 @@
+import React,  { createContext, useState, useEffect }  from 'react';
+
+const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+
+  const [users, setUsers] = useState({}) // Store users as { username: token }
+  const [curUser, setCurUser] = useState('') // Track current user(username) in this tab
+
+  useEffect(()=> {
+    // Load all logged-in users from localStorage
+    const storedUsers = JSON.parse(localStorage.getItem('loggedInUsers')) || {}
+    setUsers(storedUsers)
+
+    // Load current user from sessionStorage(tab-specific)
+    const storedCurUser = sessionStorage.getItem('curUser')
+    if(storedCurUser && storedUsers[storedCurUser]){
+      setCurUser(storedCurUser)
+    }
+
+  },[])
+
+  const login = (username, token)=> {
+    setUsers(prevUsers => {
+     const updatedUsers = {...prevUsers, [username]:token}
+     localStorage.setItem('loggedInUsers', JSON.stringify(updatedUsers))
+     return updatedUsers
+    })
+    setCurUser(username)
+    sessionStorage.setItem('curUser', username)
+    localStorage.setItem(`token_${username}`, token)
+  }
+
+  const logout = (username)=> {
+    setUsers(prevUsers => {
+      const updatedUsers = {...prevUsers}
+      delete updatedUsers[username]
+      localStorage.setItem('loggedInUsers', JSON.stringify(updatedUsers))
+      return updatedUsers
+    });
+
+    if (curUser === username){
+      setCurUser('');
+      sessionStorage.removeItem('curUser')
+      localStorage.removeItem(`token_${username}`)
+    }
+
+  }
+
+  return (
+      <AuthContext.Provider value={{users, curUser, login, logout}}>
+          {children}
+      </AuthContext.Provider>
+  );
+}
+
+export { AuthProvider , AuthContext }
